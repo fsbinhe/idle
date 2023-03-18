14facecede5c7d027bcf3a4546f0cd6ea7037dd5
hebin <fs.bin.he@gmail.com>
2023-03-18 22:34:39 +0800

    enable raft api

--- client/async/main.cc
+++ client/async/main.cc
@@ -11,15 +11,15 @@
 
 int main(int argc, char *argv[])
 {
-  async::v1::EchoRequest req;
-  async::v1::EchoResponse resp;
-  req.set_id(10086);
 
+  async::v1::CreateTaskRequest createTaskReq;
+  async::v1::CreateTaskResponse createTaskResp;
+  createTaskReq.set_handler_id(100);
+  createTaskReq.set_body("name: hebin");
   auto channel = grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials());
   std::unique_ptr<async::v1::Async::Stub> stub = async::v1::Async::NewStub(channel);
   grpc::ClientContext context;
-  grpc::Status status = stub->Echo(&context, req, &resp);
-
-  std::cout << "response id=" << resp.id() << std::endl;
+  grpc::Status status = stub->CreateTask(&context, createTaskReq, &createTaskResp);
+  std::cout << "status=" << status.error_code() << ",message=" << status.error_details() << std::endl;
   return 0;
 }
--- src/asyncd.cc
+++ src/asyncd.cc
@@ -4,28 +4,67 @@
 #include <async/v1/async.pb.h>
 #include <async/v1/async.grpc.pb.h>
 
+#include <raft/v1/raft.pb.h>
+#include <raft/v1/raft.grpc.pb.h>
+
 #include <grpc/grpc.h>
 #include <grpcpp/server_builder.h>
+
+#include <iostream>
+#include "leveldb/db.h"
+
+#include <folly/dynamic.h>
+
+class RaftServiceImpl final : public raft::v1::Raft::Service
+{
+public:
+  RaftServiceImpl() {}
+
+  virtual ::grpc::Status VoteRequest(::grpc::ServerContext *ctx, const raft::v1::VoteRequest *req, raft::v1::VoteResponse *resp)
+  {
+    return grpc::Status::OK;
+  }
+};
 
 class AsyncServiceImpl final : public async::v1::Async::Service
 {
 public:
-  virtual ::grpc::Status Echo(::grpc::ServerContext *ctx, const async::v1::EchoRequest *req, async::v1::EchoResponse *resp)
+  AsyncServiceImpl()
   {
-    resp->set_id(req->id());
+    leveldb::Options options;
+    options.create_if_missing = true;
+    leveldb::Status status = leveldb::DB::Open(options, "/tmp/asyncddb", &db);
+    assert(status.ok());
+    LOG(ERROR) << "asyncd: storage initiliased";
+  }
+
+  virtual ::grpc::Status CreateTask(::grpc::ServerContext *ctx, const async::v1::CreateTaskRequest *req, async::v1::CreateTaskResponse *resp)
+  {
+    LOG(ERROR) << "asyncd::CreateTask";
+    resp->set_id("10087");
+    resp->set_error_code(0);
+    resp->set_message("OK");
     return grpc::Status::OK;
   }
+
+private:
+  leveldb::DB *db;
 };
 
 int main(int argc, char *argv[])
 {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
+  LOG(ERROR) << "asyncd::bootstrap"
+             << "version=0.0.1";
   grpc::ServerBuilder builder;
   builder.AddListeningPort("0.0.0.0:50051", grpc::InsecureServerCredentials());
   AsyncServiceImpl service;
   builder.RegisterService(&service);
   std::unique_ptr<grpc::Server> server{builder.BuildAndStart()};
+
+  folly::dynamic array = folly::dynamic::array("one", 2, 3.0);
+
   server->Wait();
   return 0;
 }
